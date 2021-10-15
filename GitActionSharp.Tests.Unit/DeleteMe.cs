da6de91321edc8be84c014e945a3ea1f7a420018
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-14 18:20:53 -0700

    INFRA: Setup test project

---
+++ GitActionSharp.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+// ---------------------------------------------------------------
+// Copyright (c) Shri Humrudha Jagathisun All rights reserved.
+// ---------------------------------------------------------------
+
+using Xunit;
+
+namespace GitActionSharp.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void DeleteMeTest() =>
+            Assert.True(true);
+    }
+}
