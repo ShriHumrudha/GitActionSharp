8ad6f97d753896b660da08ab7b022e759c467341
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-17 12:35:24 -0700

    BROKERS: Output Broker

---
+++ GitActionSharp/Brokers/Outputs/IOutputBroker.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Shri Humrudha Jagathisun All rights reserved.
+// ---------------------------------------------------------------
+
+namespace GitActionSharp.Brokers.Outputs
+{
+    public interface IOutputBroker
+    {
+        void GenerateFileOutput(string destinationPath, string content);
+    }
+}
---
+++ GitActionSharp/Brokers/Outputs/OutputBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Shri Humrudha Jagathisun All rights reserved.
+// ---------------------------------------------------------------
+
+using System.IO;
+
+namespace GitActionSharp.Brokers.Outputs
+{
+    public class OutputBroker : IOutputBroker
+    {
+        public void GenerateFileOutput(string destinationPath, string contents) =>
+            File.WriteAllText(destinationPath, contents);
+    }
+}
