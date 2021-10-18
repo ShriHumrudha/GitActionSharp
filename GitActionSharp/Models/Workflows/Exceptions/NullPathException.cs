1ac540f22f71e076fee2f9e857fe782a24f9b645
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-17 17:05:31 -0700

    ShouldThrowValidationExceptionOnCreateIfDestinationPathIsNull -> FAIL

---
+++ GitActionSharp/Models/Workflows/Exceptions/NullPathException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Shri Humrudha Jagathisun All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace GitActionSharp.Models.Workflows.Exceptions
+{
+    public class NullPathException : Exception
+    {
+        public NullPathException()
+            : base(message: "Path is null.")
+        { }
+    }
+}
---
+++ GitActionSharp/Models/Workflows/Exceptions/WorkflowValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Shri Humrudha Jagathisun All rights reserved.
+// ---------------------------------------------------------------
+
+using System;
+
+namespace GitActionSharp.Models.Workflows.Exceptions
+{
+    public class WorkflowValidationException : Exception
+    {
+        public WorkflowValidationException(Exception innerException)
+            : base(message: "Workflow validation exception occurred, try again.", innerException)
+        { }
+    }
+}
