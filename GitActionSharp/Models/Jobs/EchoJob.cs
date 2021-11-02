568d3b89cae9d4bcea05b56006684d35445d776e
Shri Humrudha <shri@mumswhocode.com>
2021-11-02 00:15:45 -0700

    CODE RUB: Updated jobs

--- GitActionSharp/Models/Jobs/EchoJob.cs
+++ GitActionSharp/Models/Jobs/EchoJob.cs
@@ -2,14 +2,21 @@
 // Copyright (c) Shri Humrudha Jagathisun All rights reserved.
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using GitActionSharp.Models.Tasks;
 using YamlDotNet.Serialization;
 
 namespace GitActionSharp.Models.Jobs
 {
-    public class Job
+    public class EchoJob
     {
-        public EchoJob Echo { get; set; }
+        [YamlMember(Alias = "runs-on")]
+        public string RunsOn { get; set; }
+
+        public List<GitActionTask> Steps { get; set; }
     }
 }
